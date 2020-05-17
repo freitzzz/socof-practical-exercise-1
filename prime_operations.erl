38ede6bf79826b9a137b5f6bf5630d0d755981f6
João Freitas <1160907@isep.ipp.pt>
2020-05-17 16:58:26 +0100

    Moved prime operations to own module

---
+++ prime_operations.erl
@@ -0,0 +1,23 @@
+-module(prime_operations).
+
+-export([compute_prime/1, find_prime_integers/1,
+	 is_prime/1]).
+
+is_prime(N, N, 0) -> true;
+is_prime(_, _, 0) -> false;
+is_prime(N, A, _) -> is_prime(N, A + 1, N rem (A + 1)).
+
+is_prime(0) -> false;
+is_prime(1) -> false;
+is_prime(N) -> is_prime(N, 1, 1).
+
+find_prime_integers([]) -> [];
+find_prime_integers([H | T]) ->
+    case is_prime(H) of
+      true -> [H | find_prime_integers(T)];
+      false -> find_prime_integers(T)
+    end.
+
+compute_prime(Integers) ->
+    lists:foldl(fun (X, Sum) -> X + Sum end, 0,
+		find_prime_integers(Integers)).
--- work_distribution.erl
+++ work_distribution.erl
@@ -1,23 +1,9 @@
 -module(work_distribution).
 
--export([compute_prime/1, find_prime_integers/1,
-	 handle_requests/0, is_prime/1, link_slaves/2,
+-export([handle_requests/0, link_slaves/2,
 	 spawn_slaves/2]).
 
-is_prime(N, N, 0) -> true;
-is_prime(_, _, 0) -> false;
-is_prime(N, A, _) -> is_prime(N, A + 1, N rem (A + 1)).
-
-is_prime(0) -> false;
-is_prime(1) -> false;
-is_prime(N) -> is_prime(N, 1, 1).
-
-find_prime_integers([]) -> [];
-find_prime_integers([H | T]) ->
-    case is_prime(H) of
-      true -> [H | find_prime_integers(T)];
-      false -> find_prime_integers(T)
-    end.
+-import(prime_operations, [compute_prime/1]).
 
 on_exit(Pid, Fun) ->
     spawn(fun () ->
@@ -25,10 +11,6 @@ on_exit(Pid, Fun) ->
 		  link(Pid),
 		  receive {'EXIT', Pid, _} -> Fun(Pid) end
 	  end).
-
-compute_prime(Integers) ->
-    lists:foldl(fun (X, Sum) -> X + Sum end, 0,
-		find_prime_integers(Integers)).
 
 handle_requests() ->
     receive
