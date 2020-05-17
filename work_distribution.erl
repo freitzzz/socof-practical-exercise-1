7edd5c00b89ca2622425b602b3b1aa95c95c6a19
João Freitas <1160907@isep.ipp.pt>
2020-05-17 16:06:58 +0100

    Added link_slaves function

--- work_distribution.erl
+++ work_distribution.erl
@@ -1,7 +1,8 @@
 -module(work_distribution).
 
 -export([compute_prime/1, find_prime_integers/1,
-	 is_prime/1, spawn_slaves/1]).
+	 handle_requests/0, is_prime/1, link_slaves/2,
+	 spawn_slaves/2]).
 
 is_prime(N, N, 0) -> true;
 is_prime(_, _, 0) -> false;
@@ -18,11 +19,27 @@ find_prime_integers([H | T]) ->
       false -> find_prime_integers(T)
     end.
 
+on_exit(Pid, Fun) ->
+    spawn(fun () ->
+		  process_flag(trap_exit, true),
+		  link(Pid),
+		  receive {'EXIT', Pid, _} -> Fun(Pid) end
+	  end).
+
 compute_prime(Integers) ->
     lists:foldl(fun (X, Sum) -> X + Sum end, 0,
 		find_prime_integers(Integers)).
 
-spawn_slaves(0) -> [];
-spawn_slaves(N) ->
-    spawn_slaves(N - 1) ++
-      [spawn(fun work_distribution:compute_prime/1)].
+handle_requests() ->
+    receive
+      {FromPid, Integers} ->
+	  FromPid ! compute_prime(Integers), handle_requests()
+    end.
+
+spawn_slaves(0, _) -> [];
+spawn_slaves(N, Fun) ->
+    spawn_slaves(N - 1, Fun) ++ [spawn(Fun)].
+
+link_slaves([], _) -> [];
+link_slaves([H | T], Fun) ->
+    [on_exit(H, Fun) | link_slaves(T, Fun)].
