fb60b6424e8cd083a8a43f622d69036a574177d6
João Freitas <1160907@isep.ipp.pt>
2020-05-17 06:36:58 +0100

    Added is_prime predicate

--- work_distribution.erl
+++ work_distribution.erl
@@ -1,6 +1,14 @@
 -module(work_distribution).
 
--export([compute_prime/1, spawn_slaves/1]).
+-export([compute_prime/1, is_prime/1, spawn_slaves/1]).
+
+is_prime(N, N, 0) -> true;
+is_prime(_, _, 0) -> false;
+is_prime(N, A, _) -> is_prime(N, A + 1, (A + 1) rem N).
+
+is_prime(0) -> false;
+is_prime(1) -> false;
+is_prime(N) -> is_prime(N, 1, 1).
 
 compute_prime(Integers) -> Integers.
 
