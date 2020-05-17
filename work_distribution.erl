8e0e45a1834320d526efc2ae719b95858a3f7b3f
João Freitas <1160907@isep.ipp.pt>
2020-05-17 07:07:46 +0100

    Fix is_prime not dividing the correct quotient, fix find_prime_integers not returning integers list and implemented compute_prime function

--- work_distribution.erl
+++ work_distribution.erl
@@ -1,19 +1,26 @@
 -module(work_distribution).
 
--export([compute_prime/1, is_prime/1, spawn_slaves/1]).
+-export([compute_prime/1, find_prime_integers/1,
+	 is_prime/1, spawn_slaves/1]).
 
 is_prime(N, N, 0) -> true;
 is_prime(_, _, 0) -> false;
-is_prime(N, A, _) -> is_prime(N, A + 1, (A + 1) rem N).
+is_prime(N, A, _) -> is_prime(N, A + 1, N rem (A + 1)).
 
 is_prime(0) -> false;
 is_prime(1) -> false;
 is_prime(N) -> is_prime(N, 1, 1).
 
-find_prime_integers(Integers) ->
-    [is_prime(X) || X <- Integers].
+find_prime_integers([]) -> [];
+find_prime_integers([H | T]) ->
+    case is_prime(H) of
+      true -> [H | find_prime_integers(T)];
+      false -> find_prime_integers(T)
+    end.
 
-compute_prime(Integers) -> Integers.
+compute_prime(Integers) ->
+    lists:foldl(fun (X, Sum) -> X + Sum end, 0,
+		find_prime_integers(Integers)).
 
 spawn_slaves(0) -> [];
 spawn_slaves(N) ->
