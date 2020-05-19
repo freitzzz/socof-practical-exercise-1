05f404f274b194e6187e227c0a61992e45108486
João Freitas <1160907@isep.ipp.pt>
2020-05-19 10:35:47 +0100

    Dead code removal on master

--- master.erl
+++ master.erl
@@ -61,20 +61,6 @@ distribute_parcels(Master, [HP | TP], [HS | TS]) ->
     HS ! {Master, {compute_prime, HP}},
     distribute_parcels(Master, TP, TS).
 
-% split_integers_in_parcels(_, 0) -> [];
-% split_integers_in_parcels(Integers, 1) -> [Integers];
-% split_integers_in_parcels(Integers, 2) ->
-%     {L1, L2} = lists:split(2, Integers), [L1, L2];
-% split_integers_in_parcels(Integers, 3) ->
-%     {L1, L2} = lists:split(2, Integers),
-%     {L11, L12} = lists:split(1, L1),
-%     [L11, L12, L2];
-% split_integers_in_parcels(Integers, NParcels) ->
-%     A = round(length(Integers) / 2),
-%     {L1, L2} = lists:split(A, Integers),
-%     split_integers_in_parcels(L1, NParcels - 2) ++
-%       split_integers_in_parcels(L2, NParcels - 2).
-
 split_integers_in_parcels(Integers) ->
     case length(Integers) =< 10 of
       true -> [Integers];
