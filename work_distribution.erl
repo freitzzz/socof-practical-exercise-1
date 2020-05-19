0058b4b439de84983b58f466c23b9521c71aaacf
João Freitas <1160907@isep.ipp.pt>
2020-05-19 13:52:04 +0100

    Added load test on work_distribution module

--- work_distribution.erl
+++ work_distribution.erl
@@ -1,27 +1,16 @@
 -module(work_distribution).
 
--export([handle_requests/0, link_slaves/2,
-	 spawn_slaves/2]).
+-export([load_test/0]).
 
--import(prime_operations, [compute_prime/1]).
-
-on_exit(Pid, Fun) ->
-    spawn(fun () ->
-		  process_flag(trap_exit, true),
-		  link(Pid),
-		  receive {'EXIT', Pid, _} -> Fun(Pid) end
-	  end).
-
-handle_requests() ->
-    receive
-      {FromPid, Integers} ->
-	  FromPid ! compute_prime(Integers), handle_requests()
-    end.
-
-spawn_slaves(0, _) -> [];
-spawn_slaves(N, Fun) ->
-    spawn_slaves(N - 1, Fun) ++ [spawn(Fun)].
-
-link_slaves([], _) -> [];
-link_slaves([H | T], Fun) ->
-    [on_exit(H, Fun) | link_slaves(T, Fun)].
+load_test() ->
+    master:new(m1),
+    m1 ! {self(), {spawn_slaves, 5}},
+    Runs = lists:seq(1, 1000),
+    lists:foreach(fun (_) ->
+			  io:format("Run~n"),
+			  RandomNumbers = [rand:uniform(100)
+					   || _ <- lists:seq(1, 1000)],
+			  m1 ! {self(), {distribute_load, RandomNumbers}},
+			  io:format("After send message~n")
+		  end,
+		  Runs).
