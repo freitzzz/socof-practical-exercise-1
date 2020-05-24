30bebb45029018e8435c530ee1c42d59e8416d17
João Freitas <1160907@isep.ipp.pt>
2020-05-24 01:29:31 +0100

    Prepare work_distribution for deliver

--- work_distribution.erl
+++ work_distribution.erl
@@ -2,18 +2,35 @@
 
 -export([load_test/0]).
 
+register_receiver(Alias) ->
+    case whereis(Alias) of
+      undefined ->
+	  register(Alias,
+		   spawn(fun () -> receive_results_handler() end));
+      _ -> ok
+    end.
+
+receive_results_handler() ->
+    receive
+      {result_compute_prime, ResultComputePrime} ->
+	  io:format("Result Prime: ~w~n", [ResultComputePrime]),
+	  receive_results_handler()
+    end.
+
 load_test() ->
     case whereis(m1) of
-      undefined -> master:new(m1);
+      undefined ->
+	  master:new(m1),
+	  m1 ! {work_receiver, {spawn_slaves, 20}},
+	  register_receiver(work_receiver);
       _ -> ok
     end,
-    m1 ! {self(), {spawn_slaves, 20}},
     Runs = lists:seq(1, 100),
+    io:format("Starting load test...~n"),
     lists:foreach(fun (_) ->
-			  io:format("Run~n"),
-			  RandomNumbers = [rand:uniform(100)
+			  RandomNumbers = [rand:uniform(1000)
 					   || _ <- lists:seq(1, 1000)],
-			  m1 ! {self(), {compute_prime, RandomNumbers}},
-			  io:format("After send message~n")
+			  m1 ! {work_receiver, {compute_prime, RandomNumbers}}
 		  end,
-		  Runs).
+		  Runs),
+    io:format("All load distributed.~n").
